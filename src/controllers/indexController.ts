3e90e2d70c419c930029ceb0a125bc981ecb6995
nerdeveloper <odirionye@gmail.com>
2019-09-06 04:45:01 +0100

    add contact

--- src/controllers/indexController.ts
+++ src/controllers/indexController.ts
@@ -1,9 +1,66 @@
 import { Request, Response, NextFunction } from "express";
+import { validationResult, } from 'express-validator';
+const sgTransport = require('nodemailer-sendgrid-transport');
+import nodemailer from 'nodemailer';
 
+const transporter = nodemailer.createTransport(
+  sgTransport({
+    service: 'SendGrid',
+    auth: {
+      api_key: "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",
+    }
+  }),
+)
 export const home = (req: Request, res: Response) => {
-    res.render('home', {title: 'Home'})
+  res.render('home', { title: 'Home' })
 }
 
 export const contact = (req: Request, res: Response) => {
-    res.render('contact')
+  res.render('contact')
 }
+export const contactForm = (req: Request, res: Response, ) => {
+
+  try {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      //@ts-ignore
+      req.flash('error', errors.array().map(err => err.msg));
+      res.render('contact', {
+        title: 'Contact',
+        body: req.body,
+        flashes: req.flash()
+      });
+    } else {
+
+
+
+      const mailOptions = {
+        to: 'odirionye@gmail.com',
+        from: `${req.body.name} <${req.body.email}>`,
+        subject: 'Contact Form',
+        text: req.body.message
+      }
+
+      transporter.sendMail(mailOptions, (err, data) => {
+        if (err) {
+          console.log('error')
+          req.flash("error", "Error! We did Something wrong");
+          res.redirect('/contact');
+
+        }
+        else {
+          req.flash("success", "Email has been sent successfully!");
+          res.redirect('/contact');
+          console.log(data);
+        }
+
+      });
+    }
+    return;
+  } catch (e) {
+    res.redirect('/contact');
+  }
+
+}
+
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,38 @@
 import express from 'express';
- //import {Request, Response} from 'express';
+import {Request, Response, NextFunction} from 'express'
+import {check, sanitizeBody, } from 'express-validator';
+
 const router = express.Router();
 //import * as userController from '../controllers/userController';
 import * as indexController from '../controllers/indexController';
 
 router.get('/',  indexController.home)
 router.get('/contact', indexController.contact) 
+
+router.post('/contact',
+[
+    /**Check the form and validated it before submitting  */
+    sanitizeBody('name'),
+    check('name', 'Name cannot be blank').not().isEmpty(),
+    sanitizeBody('surname'),
+    check('surname', 'Name cannot be blank').not().isEmpty(),
+    check('need', 'Your framework cannot be empty').not().isEmpty(),
+    check('email', 'Email is not valid').isEmail(),
+    check('email').normalizeEmail({
+        gmail_remove_subaddress: false, // correct
+        outlookdotcom_remove_subaddress: false,
+        gmail_remove_dots: false,
+        icloud_remove_subaddress: false,
+
+    }),
+
+    check('message', 'Message cannot be blank').not().isEmpty(),
+
+],
+(req: Request, res: Response) => {
+    indexController.contactForm(req, res);
+});
+
 
 
 
